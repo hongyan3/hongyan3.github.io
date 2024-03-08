be277404612acb49d04970763ad683ba5c67cf1d
xiyuan <Hongyan.Zhang3@outlook.com>
2024-03-08 10:07:25 +0800

    init

---
+++ docs/.vitepress/nav.ts
@@ -0,0 +1,12 @@
+export default function () {
+  return [
+    {
+      text: '学习笔记',
+      items: [
+        { text: 'Java面试笔记', link: '/resource/code/java', activeMatch: '/resource/code/java' },
+        { text: 'Go', link: '/notes/go', activeMatch: '/notes/go' },
+      ],
+      activeMatch: '/notes',
+    },
+  ]
+}
